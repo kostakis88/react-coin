179a1aaf0db841d5370188181695066c32eefa7d
kostakis88 <kostasdallas2010@hotmail.com>
2019-01-22 00:28:25 +0200

    add app and header component

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/common/Header';
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <h1>React Coin</h1>
+            <p>Up to date crypto currencies financial data</p>
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
